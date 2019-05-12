5a20322dd496e2d41bc8611a4ddc4a82940eb2a3
Drew Stimson <drewstimson@Drews-MacBook-Air.local>
2019-05-12 16:33:29 -0400

    begin bringing in twitter api

--- positive_twitter/browse/views.py
+++ positive_twitter/browse/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import twitter
+
+api = twitter.Api(consumer_key=[consumer key],
+                  consumer_secret=[consumer secret],
+                  access_token_key=[access token],
+                  access_token_secret=[access token secret])
 
 
 def index(request):
